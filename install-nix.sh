afacb13787ff22a2748eb377898ea63c37518748
epi <epibar052@gmail.com>
2020-10-25 07:38:48 -0500

    install script for nix now adds emoji font

--- install-nix.sh
+++ install-nix.sh
@@ -11,31 +11,47 @@ LIN32_URL="${BASE_URL}/${LIN32_ZIP}"
 LIN64_ZIP=x86_64-linux-feroxbuster.zip
 LIN64_URL="${BASE_URL}/${LIN64_ZIP}"
 
+EMOJI_ZIP=NotoColorEmoji-unhinted.zip
+EMOJI_URL=https://noto-website-2.storage.googleapis.com/pkgs/NotoColorEmoji-unhinted.zip
+
 echo "[+] Installing feroxbuster!"
 
 if [[ "$(uname)" == "Darwin" ]]; then
     echo "[=] Found MacOS, downloading from ${MAC_URL}"
 
     curl -sLO "${MAC_URL}"
-    unzip "${MAC_ZIP}" > /dev/null
+    unzip -o "${MAC_ZIP}" > /dev/null
     rm "${MAC_ZIP}"
 elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
     if [[ $(getconf LONG_BIT) == 32 ]]; then
         echo "[=] Found 32-bit Linux, downloading from ${LIN32_URL}"
 
         curl -sLO "${LIN32_URL}"
-        unzip "${LIN32_ZIP}" > /dev/null
+        unzip -o "${LIN32_ZIP}" > /dev/null
         rm "${LIN32_ZIP}"
     else
         echo "[=] Found 64-bit Linux, downloading from ${LIN64_URL}"
 
         curl -sLO "${LIN64_URL}"
-        unzip "${LIN64_ZIP}" > /dev/null
+        unzip -o "${LIN64_ZIP}" > /dev/null
         rm "${LIN64_ZIP}"
     fi
+
+    if [[ $(lsb_release -d) =~ [Kk]ali ]]; then
+        echo "[=] Found Kali, installing Noto Emoji Font"
+        mkdir -p ~/.fonts
+        pushd ~/.fonts 2>&1 >/dev/null
+        curl -sLO "${EMOJI_URL}"
+        unzip -o "${EMOJI_ZIP}" >/dev/null
+        rm "${EMOJI_ZIP}"
+        popd 2>&1 >/dev/null
+        echo "[+] Noto Emoji Font installed"
+    fi 
 fi
 
 chmod +x ./feroxbuster
 
 echo "[+] Installed feroxbuster version $(./feroxbuster -V)"
+
+
 
