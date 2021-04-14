d59af94f6287a856fe11befda9a4b5661be2a2ca
Restyled.io <commits@restyled.io>
2021-04-14 17:37:26 +0000

    Restyled by shellharden

--- install-nix.sh
+++ install-nix.sh
@@ -3,13 +3,13 @@
 BASE_URL=https://github.com/epi052/feroxbuster/releases/latest/download
 
 MAC_ZIP=x86_64-macos-feroxbuster.zip
-MAC_URL="${BASE_URL}/${MAC_ZIP}"
+MAC_URL="$BASE_URL/$MAC_ZIP"
 
 LIN32_ZIP=x86-linux-feroxbuster.zip
-LIN32_URL="${BASE_URL}/${LIN32_ZIP}"
+LIN32_URL="$BASE_URL/$LIN32_ZIP"
 
 LIN64_ZIP=x86_64-linux-feroxbuster.zip
-LIN64_URL="${BASE_URL}/${LIN64_ZIP}"
+LIN64_URL="$BASE_URL/$LIN64_ZIP"
 
 EMOJI_URL=https://gist.github.com/epi052/8196b550ea51d0907ad4b93751b1b57d/raw/6112c9f32ae07922983fdc549c54fd3fb9a38e4c/NotoColorEmoji.ttf
 
@@ -25,24 +25,24 @@ else
 fi
 
 if [[ "$(uname)" == "Darwin" ]]; then
-    echo "[=] Found MacOS, downloading from ${MAC_URL}"
+    echo "[=] Found MacOS, downloading from $MAC_URL"
 
-    curl -sLO "${MAC_URL}"
-    unzip -o "${MAC_ZIP}" > /dev/null
-    rm "${MAC_ZIP}"
+    curl -sLO "$MAC_URL"
+    unzip -o "$MAC_ZIP" > /dev/null
+    rm "$MAC_ZIP"
 elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
     if [[ $(getconf LONG_BIT) == 32 ]]; then
-        echo "[=] Found 32-bit Linux, downloading from ${LIN32_URL}"
+        echo "[=] Found 32-bit Linux, downloading from $LIN32_URL"
 
-        curl -sLO "${LIN32_URL}"
-        unzip -o "${LIN32_ZIP}" > /dev/null
-        rm "${LIN32_ZIP}"
+        curl -sLO "$LIN32_URL"
+        unzip -o "$LIN32_ZIP" > /dev/null
+        rm "$LIN32_ZIP"
     else
-        echo "[=] Found 64-bit Linux, downloading from ${LIN64_URL}"
+        echo "[=] Found 64-bit Linux, downloading from $LIN64_URL"
 
-        curl -sLO "${LIN64_URL}"
-        unzip -o "${LIN64_ZIP}" > /dev/null
-        rm "${LIN64_ZIP}"
+        curl -sLO "$LIN64_URL"
+        unzip -o "$LIN64_ZIP" > /dev/null
+        rm "$LIN64_ZIP"
     fi
 
     if [[ -e ~/.fonts/NotoColorEmoji.ttf ]]; then
@@ -52,7 +52,7 @@ elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
         mkdir -p ~/.fonts
         pushd ~/.fonts 2>&1 >/dev/null
 
-        curl -sLO "${EMOJI_URL}"
+        curl -sLO "$EMOJI_URL"
 
         fc-cache -f -v >/dev/null
 
