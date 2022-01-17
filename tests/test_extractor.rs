1ef5bc288a2504ba80a435d0cc71769d7ca4af41
godylockz <81207744+godylockz@users.noreply.github.com>
2022-01-16 22:34:37 -0500

    Fix tests back after removing extra prints

--- tests/test_extractor.rs
+++ tests/test_extractor.rs
@@ -466,7 +466,7 @@ fn extractor_finds_directory_listing_links_and_displays_files() {
             .and(predicate::str::contains("22c"))
             .and(predicate::str::contains("/misc/LICENSE"))
             .and(predicate::str::contains("29c"))
-            .and(predicate::str::contains("200").count(5)),
+            .and(predicate::str::contains("200").count(3)),
     );
 
     assert_eq!(mock_root.hits(), 2);
@@ -577,7 +577,7 @@ fn extractor_finds_directory_listing_links_and_displays_files_non_recursive() {
             .not()
             .and(predicate::str::contains("/misc/LICENSE").not())
             .and(predicate::str::contains("29c").not())
-            .and(predicate::str::contains("200").count(2)),
+            .and(predicate::str::contains("200").count(1)),
     );
 
     assert_eq!(mock_root.hits(), 2);
